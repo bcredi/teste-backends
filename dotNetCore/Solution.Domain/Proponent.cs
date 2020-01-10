60af9bc16aede23398ea1c9bf99f213f54d0ccc8
Margareth Suchy <margareths@jmalucelliti.com.br>
2020-01-10 11:39:01 -0300

    Add Proponent ctor

--- dotNetCore/Solution.Domain/Proponent.cs
+++ dotNetCore/Solution.Domain/Proponent.cs
@@ -1,13 +1,23 @@
+using Solution.Domain.Events.Proponent;
 using System;
 
 namespace Solution.Domain
 {
     public class Proponent
     {
-      public Guid Id { get; set; }
-      public string Name { get; set; }
-      public int Age { get; set; }
-      public decimal MonthlyIncome { get; set; }
-      public bool IsMain { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal MonthlyIncome { get; set; }
+        public bool IsMain { get; set; }
+
+        public Proponent(ProponentAddedEvent ev)
+        {
+            Id = ev.ProponentId;
+            Name = ev.Name;
+            Age = ev.Age;
+            MonthlyIncome = ev.MonthlyIncome;
+            IsMain = ev.IsMain;
+        }
     }
 }
