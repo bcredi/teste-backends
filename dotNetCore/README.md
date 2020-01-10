aa3274cf055b34f34683fb8c3a47e8dd40ffd50e
Margareth Suchy <msuchy@suchy.local>
2020-01-09 22:27:48 -0300

    Add .net core solution skeleton

---
+++ dotNetCore/README.md
@@ -0,0 +1,18 @@
+# Teste de Back-end Bcredi
+## .net Core C#
+Para resolver o desafio em .net Core, edite o arquivo `Solution.cs`. Os testes rodam a partir do arquivo SolutionApp/Program.cs
+
+Para testar sua solução, rode:
+
+```
+cd ./dotNetCore # você precisa estar no diretório!
+dotnet build
+dotnet run -p SolutionApp/SolutionApp.csproj
+```
+
+
+Para executar os testes unitários, rode:
+```
+cd ./dotNetCore # você precisa estar no diretório!
+dotnet test
+```
---
+++ dotNetCore/SolutionApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace SolutionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string basePath = $"../test";
+
+            for (var index = 0; index < 13; index++)
+            {
+                var strIndex = index.ToString().PadLeft(3,'0');
+                var inputLines = File.ReadAllLines($"{basePath}/input/input{strIndex}.txt");
+                var outputLines = File.ReadAllLines($"{basePath}/output/output{strIndex}.txt");
+
+                var result = Solution.ProcessMessages(inputLines);
+                if (outputLines[0] == result)
+                    Console.WriteLine($"Test #{index + 1}/#13 - Passed");
+                else
+                    Console.WriteLine($"Test #{index + 1}/#13 - Failed");
+            }
+        }
+    }
+}
